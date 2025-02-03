9e51bd1fda5283f8bacb70fab0dade16b17d6e83
Alisa <bilgeir@gmail.com>
2025-02-03 20:45:52 +0300

    Task done v1

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -44,7 +44,7 @@ public class CallbackTest {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Георгий");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+75645552827");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        // driver.findElement(By.(необходимый селектор)("[имя селектора]")).click();
+        driver.findElement(By.cssSelector("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
